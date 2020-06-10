da08c5fb8b54c59633c4099c0688854645563ba4
dapingtai <zero102x@gmail.com>
2020-06-10 11:45:36 +0800

    Package AETL

--- Affy_ETL/__init__.py
+++ Affy_ETL/__init__.py
@@ -1,8 +1,1 @@
-from . import Affy_ETL
-
-# Python to R
-import rpy2.robjects as robjects
-from rpy2.robjects import r, pandas2ri
-from rpy2.robjects.packages import importr
-import os
-import pandas as pd
+from . import AETL
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Affy-ETL", # Replace with your own username
-    version="0.0.1",
+    version="0.0.6",
     author="Hung Chang",
     author_email="zero102x@gmail.com",
     description="Affydata ETL package",
@@ -13,10 +13,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dapingtai/Affy-ETL",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v2.0",
-        "Operating System :: OS Independent",
-    ],
     python_requires='>=3.6',
 )
